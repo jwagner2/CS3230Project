593161f66326c2d1db476669f7ded972a8d9492f
Jordan Wagner <jwagner2@my.westga.edu>
2022-09-27 12:23:13 -0400

    added getters and setters

--- CS3230Project/src/model/User.java
+++ CS3230Project/src/model/User.java
@@ -6,13 +6,54 @@ public class User {
 	private String lastName;
 	private String userName;
 	private String password;
+	private boolean isAdmin;
 	
-	public User(String fname, String lname, String userName, String password) {
+	public User(String fname, String lname, String userName, String password, boolean isAdmin) {
 		
 		this.firstName = fname;
 		this.lastName = lname;
 		this.userName = userName;
 		this.password = password;
+		this.isAdmin = isAdmin;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public boolean isAdmin() {
+		return this.isAdmin;
+	}
+	public void setIsAdmin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
 	}
 
 }
