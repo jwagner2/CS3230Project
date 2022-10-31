6658406ebcf8875ba67d134aed84af5c9893de64
jwagner2 <jwagner2@csvlab-vm06.uwg.westga.edu>
2022-10-31 15:44:57 -0400

    added getDoctorAvailability to appointment Dal

--- CS3230Project/src/main/java/ethos/dal/AppointmentDal.java
+++ CS3230Project/src/main/java/ethos/dal/AppointmentDal.java
@@ -1,11 +1,14 @@
 package main.java.ethos.dal;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,6 +18,8 @@ public class AppointmentDal {
     
     private String doctorApptsQuery = "select p.fname, p.lname, a.doctor_id, a.patient_id, a.appt_datetime,"
     + " a.appt_reason from appointment a join doctor d on a.doctor_id = d.doctor_id join person p on p.pid = d.pid where d.doctor_id = ?";
+    
+    private String doctorApptTimesForDateQuery = "select a.appt_datetime from appointment a where a.doctor_id = ? and DATE(a.appt_datetime) = ?";
 
     public List<Appointment> getAppointmentsByDoctorID(int doctorID) throws SQLException {
         try (Connection connection = DriverManager.getConnection(ConnectionString.CONNECTION_STRING);
@@ -38,4 +43,22 @@ public class AppointmentDal {
         }
         
     }
+    
+    public List<LocalTime> getDoctorAvailability(int doctorId, Date dateTime) throws SQLException {
+        try (Connection connection = DriverManager.getConnection(ConnectionString.CONNECTION_STRING);
+                PreparedStatement stmt = connection.prepareStatement(doctorApptTimesForDateQuery)) {
+
+                stmt.setInt(1,  doctorId);
+                stmt.setDate(2, dateTime);
+                ResultSet rs = stmt.executeQuery();
+                List<LocalTime> apptTimes = new ArrayList<LocalTime>();
+                while (rs.next()) {
+                    LocalDateTime apptDatetime = rs.getTimestamp("appt_datetime").toLocalDateTime();
+                    apptTimes.add(apptDatetime.toLocalTime());
+                }
+                return apptTimes;
+        }
+        
+    }
+    
 }
