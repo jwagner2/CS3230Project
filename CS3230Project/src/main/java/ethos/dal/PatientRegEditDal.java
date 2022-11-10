f5fe34de8f89f1cd6e3a7034b79e6d975c4f496d
jwagner2 <jwagner2@csvlab-vm09.uwg.westga.edu>
2022-11-10 13:11:08 -0500

    stored patientRegEdit sql statements as stored procedures with transactions.

--- CS3230Project/src/main/java/ethos/dal/PatientRegEditDal.java
+++ CS3230Project/src/main/java/ethos/dal/PatientRegEditDal.java
@@ -1,9 +1,11 @@
 package main.java.ethos.dal;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+
 
 import main.java.ethos.model.Patient;
 
@@ -11,14 +13,7 @@ import main.java.ethos.model.Patient;
  * The Class PatientRegEditDal.
  */
 public class PatientRegEditDal {
-
-    String editPatientInfoQuery = "update person set fname = ?, lname = ?, ssn = ?, dob = ?, "
-            + "addr1_street = ?, addr2_street = ?, addr_state = ?, addr_zip = ?, phone = ?, gender = ? where pid = ?;"
-            + "update patient set isActive = ? where pid = ?";
-
-    String registerQuery = "insert into person (fname, lname, ssn, dob, addr1_street, addr2_street, addr_state, addr_zip, phone, gender)"
-            + "values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?); SET @new_patient_pid = (select last_insert_id()); insert into patient (pid, isActive)"
-            + "values (@new_patient_pid, 1);";
+ 
 
     /**
      * Register edit patient.
@@ -27,26 +22,26 @@ public class PatientRegEditDal {
      * @throws SQLException the SQL exception
      */
     public void editPatient(Patient patient) throws SQLException {
+        
         try (Connection connection = DriverManager.getConnection(ConnectionString.CONNECTION_STRING);
-                PreparedStatement stmt = connection.prepareStatement(editPatientInfoQuery)) {
-
-            this.setStatement(patient, true, stmt);
-            int rs = stmt.executeUpdate();
-            System.out.println("rows affected = " + rs);
+                CallableStatement cstmt = connection.prepareCall("{call uspEditPatient(?,?,?,?,?,?,?,?,?,?,?,?)}");) {
+            this.setStatement(patient, true, cstmt);
+            int rs = cstmt.executeUpdate();
         }
     }
 
     public void registerPatient(Patient patient) throws SQLException {
-        try (Connection connection = DriverManager.getConnection(ConnectionString.CONNECTION_STRING);
-                PreparedStatement stmt = connection.prepareStatement(registerQuery)) {
 
-            this.setStatement(patient, false, stmt);
-            int rs = stmt.executeUpdate();
+        try (Connection connection = DriverManager.getConnection(ConnectionString.CONNECTION_STRING);
+                CallableStatement cstmt = connection.prepareCall("{call uspRegisterPatient(?,?,?,?,?,?,?,?,?,?)}");) {
+
+            this.setStatement(patient, false, cstmt);
+            int rs = cstmt.executeUpdate();
             System.out.println("rows affected = " + rs);
         }
     }
 
-    private void setStatement(Patient patient, boolean edit, PreparedStatement stmt) throws SQLException {
+    private void setStatement(Patient patient, boolean edit, CallableStatement stmt) throws SQLException {
         stmt.setString(1, patient.getFirstName());
         stmt.setString(2, patient.getLastName());
         stmt.setString(3, patient.getSsn());
@@ -60,7 +55,6 @@ public class PatientRegEditDal {
         if (edit) {
             stmt.setInt(11, patient.getPersonId());
             stmt.setBoolean(12, patient.isActive());
-            stmt.setInt(13, patient.getPersonId());
         }
     }
 
